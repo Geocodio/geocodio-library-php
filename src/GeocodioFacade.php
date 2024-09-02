709f81044eddf625b85d03b70f6f93868f8c942f
Kirill Dakhniuk <kirilldakhnyuk@gmail.com>
2024-09-02 12:03:16 +0300

    Add doc blocks to GeocodioFacade for better autocomplete

--- src/GeocodioFacade.php
+++ src/GeocodioFacade.php
@@ -5,6 +5,12 @@ namespace Geocodio;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static Geocodio setApiKey(string $apiKey)
+ * @method static Geocodio setHostname(string $hostname)
+ * @method static Geocodio setApiVersion(string $apiVersion)
+ * @method static array|object geocode(string|array $query, array $fields = [], int $limit = null, string $format = null)
+ * @method static array|object reverse(string|array $query, array $fields = [], int $limit = null, string $format = null)
+ *
  * @see \Geocodio\GeocodioClass
  */
 class GeocodioFacade extends Facade
