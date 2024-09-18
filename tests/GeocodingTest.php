eaad7c8eca1e7b6836f7ef3206df0a97b6f1fe0d
sixlive <tj@tjmiller.co>
2024-09-18 14:42:51 -0400

    Cleanup tests

--- tests/GeocodingTest.php
+++ tests/GeocodingTest.php
@@ -16,90 +16,96 @@ beforeEach(function (): void {
     }
 });
 
-it('can perform single forward geocode', function (): void {
-    $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA');
-    expect($response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+describe('Forward Geocoding', function (): void {
+    it('can perform single forward geocode', function (): void {
+        $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA');
+        expect($response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+    });
+
+    it('can perform single forward geocode with components', function (): void {
+        $response = $this->geocoder->geocode(['street' => '1109 N Highland St', 'postal_code' => '22201']);
+        expect($response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+    });
+
+    it('can perform batch forward geocode', function (): void {
+        $response = $this->geocoder->geocode([
+            '1109 N Highland St, Arlington VA',
+            '525 University Ave, Toronto, ON, Canada',
+        ]);
+
+        expect($response->results[0]->response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+        expect($response->results[1]->response->results[0]->formatted_address)->toBe('525 University Ave, Toronto, ON M5G');
+    });
+
+    it('can perform batch forward geocode with components', function (): void {
+        $response = $this->geocoder->geocode([
+            ['street' => '1109 N Highland St', 'postal_code' => '22201'],
+            ['street' => '525 University Ave', 'city' => 'Toronto', 'state' => 'Ontario', 'country' => 'Canada'],
+        ]);
+
+        expect($response->results[0]->response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+        expect($response->results[1]->response->results[0]->formatted_address)->toBe('525 University Ave, Toronto, ON M5G');
+    });
 });
 
-it('can perform single forward geocode with components', function (): void {
-    $response = $this->geocoder->geocode(['street' => '1109 N Highland St', 'postal_code' => '22201']);
-    expect($response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+describe('Reverse Geocoding', function (): void {
+    it('can perform single reverse geocode', function (): void {
+        $responseStr = $this->geocoder->reverse('38.886665,-77.094733');
+        expect($responseStr->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+
+        $responseArr = $this->geocoder->reverse([38.886665, -77.094733]);
+        expect($responseArr->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+    });
+
+    it('can perform batch reverse geocode', function (): void {
+        $responseStr = $this->geocoder->reverse([
+            '35.9746000,-77.9658000',
+            '32.8793700,-96.6303900',
+        ]);
+        expect($responseStr->results[0]->response->results[0]->formatted_address)->toBe('101 W Washington St, Nashville, NC 27856');
+        expect($responseStr->results[1]->response->results[0]->formatted_address)->toBe('3034 S 1st St, Garland, TX 75041');
+
+        $responseArr = $this->geocoder->reverse([
+            ['35.9746000', '-77.9658000'],
+            ['32.8793700', '-96.6303900'],
+        ]);
+        expect($responseArr->results[0]->response->results[0]->formatted_address)->toBe('101 W Washington St, Nashville, NC 27856');
+        expect($responseArr->results[1]->response->results[0]->formatted_address)->toBe('3034 S 1st St, Garland, TX 75041');
+    });
 });
 
-it('can perform single reverse geocode', function (): void {
-    $responseStr = $this->geocoder->reverse('38.886665,-77.094733');
-    expect($responseStr->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
+describe('Additional Features', function (): void {
+    it('can append fields', function (): void {
+        $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA', ['timezone']);
+        expect($response->results[0]->fields->timezone->abbreviation)->toBe('EST');
 
-    $responseArr = $this->geocoder->reverse([38.886665, -77.094733]);
-    expect($responseArr->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
-});
+        $response = $this->geocoder->reverse('38.886665,-77.094733', ['timezone']);
+        expect($response->results[0]->fields->timezone->abbreviation)->toBe('EST');
 
-it('can perform batch forward geocode', function (): void {
-    $response = $this->geocoder->geocode([
-        '1109 N Highland St, Arlington VA',
-        '525 University Ave, Toronto, ON, Canada',
-    ]);
+        $response = $this->geocoder->geocode(['1109 N Highland St, Arlington VA'], ['timezone']);
+        expect($response->results[0]->response->results[0]->fields->timezone->abbreviation)->toBe('EST');
 
-    expect($response->results[0]->response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
-    expect($response->results[1]->response->results[0]->formatted_address)->toBe('525 University Ave, Toronto, ON M5G');
-});
+        $response = $this->geocoder->reverse(['38.886665,-77.094733'], ['timezone']);
+        expect($response->results[0]->response->results[0]->fields->timezone->abbreviation)->toBe('EST');
+    });
 
-it('can perform batch forward geocode with components', function (): void {
-    $response = $this->geocoder->geocode([
-        ['street' => '1109 N Highland St', 'postal_code' => '22201'],
-        ['street' => '525 University Ave', 'city' => 'Toronto', 'state' => 'Ontario', 'country' => 'Canada'],
-    ]);
+    it('can use limit parameter', function (): void {
+        $response = $this->geocoder->geocode('1107 N Highland St, Arlington VA');
+        expect(count($response->results))->toBeGreaterThan(1);
 
-    expect($response->results[0]->response->results[0]->formatted_address)->toBe('1109 N Highland St, Arlington, VA 22201');
-    expect($response->results[1]->response->results[0]->formatted_address)->toBe('525 University Ave, Toronto, ON M5G');
-});
+        $response = $this->geocoder->geocode('1107 N Highland St, Arlington VA', [], 1);
+        expect(count($response->results))->toBe(1);
+    });
 
-it('can perform batch reverse geocode', function (): void {
-    $responseStr = $this->geocoder->reverse([
-        '35.9746000,-77.9658000',
-        '32.8793700,-96.6303900',
-    ]);
-    expect($responseStr->results[0]->response->results[0]->formatted_address)->toBe('101 W Washington St, Nashville, NC 27856');
-    expect($responseStr->results[1]->response->results[0]->formatted_address)->toBe('3034 S 1st St, Garland, TX 75041');
+    it('can use format simple parameter', function (): void {
+        $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA', [], null, 'simple');
+        expect($response->address)->toBe('1109 N Highland St, Arlington, VA 22201');
+        expect($response->lat)->toBe(38.886672);
+        expect($response->lng)->toBe(-77.094735);
 
-    $responseArr = $this->geocoder->reverse([
-        ['35.9746000', '-77.9658000'],
-        ['32.8793700', '-96.6303900'],
-    ]);
-    expect($responseArr->results[0]->response->results[0]->formatted_address)->toBe('101 W Washington St, Nashville, NC 27856');
-    expect($responseArr->results[1]->response->results[0]->formatted_address)->toBe('3034 S 1st St, Garland, TX 75041');
-});
-
-it('can append fields', function (): void {
-    $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA', ['timezone']);
-    expect($response->results[0]->fields->timezone->abbreviation)->toBe('EST');
-
-    $response = $this->geocoder->reverse('38.886665,-77.094733', ['timezone']);
-    expect($response->results[0]->fields->timezone->abbreviation)->toBe('EST');
-
-    $response = $this->geocoder->geocode(['1109 N Highland St, Arlington VA'], ['timezone']);
-    expect($response->results[0]->response->results[0]->fields->timezone->abbreviation)->toBe('EST');
-
-    $response = $this->geocoder->reverse(['38.886665,-77.094733'], ['timezone']);
-    expect($response->results[0]->response->results[0]->fields->timezone->abbreviation)->toBe('EST');
-});
-
-it('can use limit parameter', function (): void {
-    $response = $this->geocoder->geocode('1107 N Highland St, Arlington VA');
-    expect(count($response->results))->toBeGreaterThan(1);
-
-    $response = $this->geocoder->geocode('1107 N Highland St, Arlington VA', [], 1);
-    expect(count($response->results))->toBe(1);
-});
-
-it('can use format simple parameter', function (): void {
-    $response = $this->geocoder->geocode('1109 N Highland St, Arlington VA', [], null, 'simple');
-    expect($response->address)->toBe('1109 N Highland St, Arlington, VA 22201');
-    expect($response->lat)->toBe(38.886672);
-    expect($response->lng)->toBe(-77.094735);
-
-    $response = $this->geocoder->reverse('38.886672,-77.094735', [], null, 'simple');
-    expect($response->address)->toBe('1109 N Highland St, Arlington, VA 22201');
-    expect($response->lat)->toBe(38.886672);
-    expect($response->lng)->toBe(-77.094735);
+        $response = $this->geocoder->reverse('38.886672,-77.094735', [], null, 'simple');
+        expect($response->address)->toBe('1109 N Highland St, Arlington, VA 22201');
+        expect($response->lat)->toBe(38.886672);
+        expect($response->lng)->toBe(-77.094735);
+    });
 });
--- tests/ListTest.php
+++ tests/ListTest.php
@@ -12,269 +12,272 @@ use Geocodio\Tests\TestDoubles\TestResponse;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 
-it('throws an exception if the file doesn\'t exist', function (): void {
-    (new Geocodio)
-        ->uploadList(
-            __DIR__.'/Fixtures/not-found.csv',
+describe('Requests', function (): void {
+    it('uploads a list', function (): void {
+        $http = Client::create([
+            TestResponse::successJson(),
+        ]);
+
+        $geocodio = (new Geocodio($http->client()));
+
+        $geocodio->uploadList(
+            __DIR__.'/Fixtures/simple.csv',
             GeocodeDirection::Forward,
             '{{B}} {{C}} {{D}} {{E}}'
         );
-})->throws(GeocodioException::class, sprintf(
-    'File (%s/Fixtures/not-found.csv) not found',
-    __DIR__,
-));
 
-it('uploads a list', function (): void {
-    $http = Client::create([
-        TestResponse::successJson(),
-    ]);
+        $history = $http->history();
 
-    $geocodio = (new Geocodio($http->client()));
+        expect($history)->toHaveCount(1);
 
-    $geocodio->uploadList(
-        __DIR__.'/Fixtures/simple.csv',
-        GeocodeDirection::Forward,
-        '{{B}} {{C}} {{D}} {{E}}'
-    );
+        /** @var Request $request */
+        $request = $history[0]['request'];
+        $body = (string) $request->getBody();
 
-    $history = $http->history();
+        // Assert the API key is being sent
+        parse_str($request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
 
-    expect($history)->toHaveCount(1);
+        // Assert that the request is a POST request
+        expect($request->getMethod())->toBe('POST');
+        expect($request->getUri()->getPath())
+            ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
 
-    /** @var Request $request */
-    $request = $history[0]['request'];
-    $body = (string) $request->getBody();
+        // Assert that the Content-Type header is set correctly
+        expect($request->getHeaderLine('Content-Type'))->toContain('multipart/form-data');
 
-    // Assert the API key is being sent
-    parse_str($request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
+        // Assert that the body contains the expected multi-part form data
+        expect($body)->toContain('name="file"');
+        expect($body)->toContain('name="direction"');
+        expect($body)->toContain('name="format"');
+        expect($body)->toContain('name="callback"');
 
-    // Assert that the request is a POST request
-    expect($request->getMethod())->toBe('POST');
-    expect($request->getUri()->getPath())
-        ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
+        // Assert specific values
+        expect($body)->toContain('filename="simple.csv"');
+        expect($body)->toContain('forward'); // The value of GeocodeDirection::Forward
+        expect($body)->toContain('{{B}} {{C}} {{D}} {{E}}');
+    });
 
-    // Assert that the Content-Type header is set correctly
-    expect($request->getHeaderLine('Content-Type'))->toContain('multipart/form-data');
+    it('uploads an inline list', function (): void {
+        $http = Client::create([
+            TestResponse::successJson(),
+        ]);
 
-    // Assert that the body contains the expected multi-part form data
-    expect($body)->toContain('name="file"');
-    expect($body)->toContain('name="direction"');
-    expect($body)->toContain('name="format"');
-    expect($body)->toContain('name="callback"');
-
-    // Assert specific values
-    expect($body)->toContain('filename="simple.csv"');
-    expect($body)->toContain('forward'); // The value of GeocodeDirection::Forward
-    expect($body)->toContain('{{B}} {{C}} {{D}} {{E}}');
-});
-
-it('uploads an inline list', function (): void {
-    $http = Client::create([
-        TestResponse::successJson(),
-    ]);
-
-    $csvData = <<<'CSV'
+        $csvData = <<<'CSV'
     name,street,city,state,zip
     "Peregrine Espresso","660 Pennsylvania Ave SE",Washington,DC,20003
     "Lot 38 Espresso Bar","1001 2nd St SE",Washington,DC,20003
     CSV;
 
-    $geocodio = (new Geocodio($http->client()));
+        $geocodio = (new Geocodio($http->client()));
 
-    $geocodio->uploadInlineList(
-        $csvData,
-        'coffee-shops.csv',
-        GeocodeDirection::Forward,
-        '{{B}} {{C}} {{D}} {{E}}'
-    );
+        $geocodio->uploadInlineList(
+            $csvData,
+            'coffee-shops.csv',
+            GeocodeDirection::Forward,
+            '{{B}} {{C}} {{D}} {{E}}'
+        );
 
-    $history = $http->history();
+        $history = $http->history();
 
-    expect($history)->toHaveCount(1);
+        expect($history)->toHaveCount(1);
 
-    $request = $history[0]['request'];
-    $body = (string) $request->getBody();
+        $request = $history[0]['request'];
+        $body = (string) $request->getBody();
 
-    // Assert the API key is being sent
-    parse_str((string) $request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
+        // Assert the API key is being sent
+        parse_str((string) $request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
 
-    // Assert that the request is a POST request
-    expect($request->getMethod())->toBe('POST');
-    expect($request->getUri()->getPath())
-        ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
+        // Assert that the request is a POST request
+        expect($request->getMethod())->toBe('POST');
+        expect($request->getUri()->getPath())
+            ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
 
-    // Assert that the Content-Type header is set correctly
-    expect($request->getHeaderLine('Content-Type'))
-        ->toContain('multipart/form-data');
+        // Assert that the Content-Type header is set correctly
+        expect($request->getHeaderLine('Content-Type'))
+            ->toContain('multipart/form-data');
 
-    // Assert that the body contains the expected multi-part form data
-    expect($body)->toContain('name="file"');
-    expect($body)->toContain('name="direction"');
-    expect($body)->toContain('name="format"');
-    expect($body)->toContain('name="callback"');
+        // Assert that the body contains the expected multi-part form data
+        expect($body)->toContain('name="file"');
+        expect($body)->toContain('name="direction"');
+        expect($body)->toContain('name="format"');
+        expect($body)->toContain('name="callback"');
 
-    // Assert specific values
-    expect($body)->toContain('filename="coffee-shops.csv"');
-    expect($body)->toContain('name,street,city,state,zip');
-    expect($body)->toContain(
-        '"Peregrine Espresso","660 Pennsylvania Ave SE",Washington,DC,20003'
-    );
-    expect($body)->toContain(
-        '"Lot 38 Espresso Bar","1001 2nd St SE",Washington,DC,20003'
-    );
-    // The value of GeocodeDirection::Forward
-    expect($body)->toContain('forward');
-    expect($body)->toContain('{{B}} {{C}} {{D}} {{E}}');
+        // Assert specific values
+        expect($body)->toContain('filename="coffee-shops.csv"');
+        expect($body)->toContain('name,street,city,state,zip');
+        expect($body)->toContain(
+            '"Peregrine Espresso","660 Pennsylvania Ave SE",Washington,DC,20003'
+        );
+        expect($body)->toContain(
+            '"Lot 38 Espresso Bar","1001 2nd St SE",Washington,DC,20003'
+        );
+        // The value of GeocodeDirection::Forward
+        expect($body)->toContain('forward');
+        expect($body)->toContain('{{B}} {{C}} {{D}} {{E}}');
+    });
+
+    it('can fetch your lists', function (): void {
+        $http = Client::create([
+            TestResponse::successJson(),
+        ]);
+
+        $geocodio = (new Geocodio($http->client()));
+
+        $geocodio->lists();
+
+        expect($http->history())->toHaveCount(1);
+
+        /** @var Request */
+        $request = $http->history()[0]['request'];
+
+        // Assert the API key is being sent
+        parse_str($request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
+
+        // Assert path and method
+        expect($request->getUri()->getPath())
+            ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
+        expect($request->getMethod())->toBe('GET');
+
+        // Assert nothing is sent in the body
+        expect((string) $request->getBody())->toBeEmpty();
+    });
+
+    it('can delete lists', function (): void {
+        $http = Client::create([
+            new Response(200, body: json_encode([])),
+        ]);
+
+        $geocodio = (new Geocodio($http->client()));
+
+        $geocodio->deleteList(11950669);
+
+        /** @var Request */
+        $request = $http->history()[0]['request'];
+
+        // Assert the API key is being sent
+        parse_str($request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
+
+        // Assert path and method
+        expect($request->getMethod())->toBe('DELETE');
+        expect($request->getUri()->getPath())
+            ->toBe(sprintf('/%s/lists/11950669', $geocodio->apiVersion()));
+
+        // Assert nothing is sent in the body
+        expect((string) $request->getBody())->toBeEmpty();
+    });
+
+    it('can fetch list status', function (): void {
+        $http = Client::create([
+            TestResponse::successJson(),
+        ]);
+
+        $geocodio = (new Geocodio($http->client()));
+
+        $geocodio->listStatus(11950669);
+
+        /** @var Request */
+        $request = $http->history()[0]['request'];
+
+        // Assert the API key is being sent
+        parse_str($request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
+
+        // Assert method and path
+        expect($request->getMethod())->toBe('GET');
+        expect($request->getUri()->getPath())
+            ->toBe(sprintf('/%s/lists/11950669', $geocodio->apiVersion()));
+
+        // Assert no body is sent
+        expect((string) $request->getBody())->toBeEmpty();
+    });
+    it('can download a list to a location', function (): void {
+        $temp = tmpfile();
+        $path = stream_get_meta_data($temp)['uri'];
+        fclose($temp);
+
+        $contents = file_get_contents(__DIR__.'/Fixtures/download.csv');
+
+        $http = Client::create([
+            new Response(200, [], $contents),
+        ]);
+
+        $geocodio = (new Geocodio($http->client()));
+
+        $geocodio->downloadList(11951418, $path);
+
+        $history = $http->history();
+
+        expect($history)->toHaveCount(1);
+
+        /** @var Request $request */
+        $request = $history[0]['request'];
+
+        // Assert the API key is being sent
+        parse_str($request->getUri()->getQuery(), $query);
+        expect($query['api_key'])->toBeString();
+
+        // Assert file contents
+        expect($path)->toBeFile();
+        expect(file_get_contents($path))->toBe($contents);
+
+        // Cleanup temp file
+        unlink($path);
+    });
 });
 
-it('can fetch your lists', function (): void {
-    $http = Client::create([
-        TestResponse::successJson(),
-    ]);
+describe('Exception handling', function (): void {
+    it('throws exception when uploading from invalid file', function (): void {
+        $http = Client::create([
+            TestResponse::invalidData(),
+        ]);
 
-    $geocodio = (new Geocodio($http->client()));
+        $geocodio = (new Geocodio($http->client()));
 
-    $geocodio->lists();
-
-    expect($http->history())->toHaveCount(1);
-
-    /** @var Request */
-    $request = $http->history()[0]['request'];
-
-    // Assert the API key is being sent
-    parse_str($request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
-
-    // Assert path and method
-    expect($request->getUri()->getPath())
-        ->toBe(sprintf('/%s/lists', $geocodio->apiVersion()));
-    expect($request->getMethod())->toBe('GET');
-
-    // Assert nothing is sent in the body
-    expect((string) $request->getBody())->toBeEmpty();
-});
-
-it('can delete lists', function (): void {
-    $http = Client::create([
-        new Response(200, body: json_encode([])),
-    ]);
-
-    $geocodio = (new Geocodio($http->client()));
-
-    $geocodio->deleteList(11950669);
-
-    /** @var Request */
-    $request = $http->history()[0]['request'];
-
-    // Assert the API key is being sent
-    parse_str($request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
-
-    // Assert path and method
-    expect($request->getMethod())->toBe('DELETE');
-    expect($request->getUri()->getPath())
-        ->toBe(sprintf('/%s/lists/11950669', $geocodio->apiVersion()));
-
-    // Assert nothing is sent in the body
-    expect((string) $request->getBody())->toBeEmpty();
-});
-
-it('can fetch list status', function (): void {
-    $http = Client::create([
-        TestResponse::successJson(),
-    ]);
-
-    $geocodio = (new Geocodio($http->client()));
-
-    $geocodio->listStatus(11950669);
-
-    /** @var Request */
-    $request = $http->history()[0]['request'];
-
-    // Assert the API key is being sent
-    parse_str($request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
-
-    // Assert method and path
-    expect($request->getMethod())->toBe('GET');
-    expect($request->getUri()->getPath())
-        ->toBe(sprintf('/%s/lists/11950669', $geocodio->apiVersion()));
-
-    // Assert no body is sent
-    expect((string) $request->getBody())->toBeEmpty();
-});
-
-it('throws exception when uploading from invalid file', function (): void {
-    $http = Client::create([
-        TestResponse::invalidData(),
-    ]);
-
-    $geocodio = (new Geocodio($http->client()));
-
-    $geocodio->uploadList(
-        __DIR__.'/Fixtures/invalid.csv',
-        GeocodeDirection::Forward,
-        '{{B}} {{C}} {{D}} {{E}}'
+        $geocodio->uploadList(
+            __DIR__.'/Fixtures/invalid.csv',
+            GeocodeDirection::Forward,
+            '{{B}} {{C}} {{D}} {{E}}'
+        );
+    })->throws(
+        GeocodioException::class,
+        'Request Error: Uploaded spreadsheet appears to be empty or unreadable'
     );
-})->throws(
-    GeocodioException::class,
-    'Request Error: Uploaded spreadsheet appears to be empty or unreadable'
-);
 
-it('throws exception when uploading from invalid inline file', function (): void {
-    $http = Client::create([
-        TestResponse::invalidData(),
-    ]);
+    it('throws exception when uploading from invalid inline file', function (): void {
+        $http = Client::create([
+            TestResponse::invalidData(),
+        ]);
 
-    $geocodio = (new Geocodio($http->client()));
+        $geocodio = (new Geocodio($http->client()));
 
-    $csvData = <<<'CSV'
+        $csvData = <<<'CSV'
     name,street,city,state,zip
     CSV;
 
-    $geocodio->uploadInlineList(
-        $csvData,
-        'coffee-shopts.csv',
-        GeocodeDirection::Forward,
-        '{{B}} {{C}} {{D}} {{E}}'
+        $geocodio->uploadInlineList(
+            $csvData,
+            'coffee-shopts.csv',
+            GeocodeDirection::Forward,
+            '{{B}} {{C}} {{D}} {{E}}'
+        );
+    })->throws(
+        GeocodioException::class,
+        'Request Error: Uploaded spreadsheet appears to be empty or unreadable'
     );
-})->throws(
-    GeocodioException::class,
-    'Request Error: Uploaded spreadsheet appears to be empty or unreadable'
-);
 
-it('can download a list to a location', function (): void {
-    $temp = tmpfile();
-    $path = stream_get_meta_data($temp)['uri'];
-    fclose($temp);
-
-    $contents = file_get_contents(__DIR__.'/Fixtures/download.csv');
-
-    $http = Client::create([
-        new Response(200, [], $contents),
-    ]);
-
-    $geocodio = (new Geocodio($http->client()));
-
-    $geocodio->downloadList(11951418, $path);
-
-    $history = $http->history();
-
-    expect($history)->toHaveCount(1);
-
-    /** @var Request $request */
-    $request = $history[0]['request'];
-
-    // Assert the API key is being sent
-    parse_str($request->getUri()->getQuery(), $query);
-    expect($query['api_key'])->toBeString();
-
-    // Assert file contents
-    expect($path)->toBeFile();
-    expect(file_get_contents($path))->toBe($contents);
-
-    // Cleanup temp file
-    unlink($path);
+    it('throws an exception if the file doesn\'t exist', function (): void {
+        (new Geocodio)
+            ->uploadList(
+                __DIR__.'/Fixtures/not-found.csv',
+                GeocodeDirection::Forward,
+                '{{B}} {{C}} {{D}} {{E}}'
+            );
+    })->throws(GeocodioException::class, sprintf(
+        'File (%s/Fixtures/not-found.csv) not found',
+        __DIR__,
+    ));
 });
